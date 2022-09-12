ff0638ee80f2f546ced269687ea2ae62a3c97abe
RaeHoon Baek <bmp117@naver.com>
2022-09-12 22:15:15 +0900

    Drink 예제 - Content 설정하기

---
+++ Drink/UNNotificationCenter.swift
@@ -0,0 +1,21 @@
+//
+//  UNNotificationCenter.swift
+//  Drink
+//
+//  Created by 백래훈 on 2022/09/12.
+//
+
+import Foundation
+import UserNotifications
+
+extension UNUserNotificationCenter {
+    
+    func addNotificationRequest(by alert: Alert) {
+        let content = UNMutableNotificationContent()
+        content.title = "물 마실 시간이에요💦"
+        content.body = "세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5 ~ 2L 입니다!"
+        content.sound = .default
+        content.badge = 1
+        
+    }
+}
